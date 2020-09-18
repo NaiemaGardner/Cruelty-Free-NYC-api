b6eaf8519334ed42f8cd2c88c5c6aee2df3bbbf7
Naiema Gardner <naiema.d.gardner@gmail.com>
2020-09-17 21:29:12 -0400

    Card Routes Commit
    Updated show and update routes

--- app/routes/card_routes.js
+++ app/routes/card_routes.js
@@ -61,7 +61,7 @@ router.get('/cards/:category', requireToken, (req, res, next) => {
 
 // SHOW
 // GET /cards/5a7db6c74d55bc51bdf39793
-router.get('/cards/:id', requireToken, (req, res, next) => {
+router.get('/cards/:catgory/:id', requireToken, (req, res, next) => {
   // req.params.id will be set based on the `:id` in the route
   Card.findById(req.params.id)
     .then(handle404)
